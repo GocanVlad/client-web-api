c4be46e117a627c2b2a5c2d725c8e5aff108e1e7
unknown <vladgocan@yahoo.com>
2021-12-12 16:31:36 +0200

    Am dat commit

--- Client/scriptRating.js
+++ Client/scriptRating.js
@@ -1,10 +1,12 @@
+'use strict'
 const baseUrl = "http://localhost:1028/api/movies/";
-
+let currentId
 async function getAllMovies() {
   await fetch(baseUrl)
     .then((res) => res.json())
     .then((data) => {
       renderMovie(data);
+      console.log(data) 
     })
     .catch((error) => {
       console.log(error);
@@ -31,9 +33,7 @@ const renderMovie = function (data) {
     `;
   for (let i = 0; i < data.length; i++) {
     listBody += ` 
-    
       <option id="${data[i].id}" value="${data[i].name}">${data[i].name}</option>`;
-    getAllRatings(data[i].id);
   }
 
   document.getElementById("div1").innerHTML = listBody;
@@ -46,24 +46,41 @@ const renderRating = function (data) {
       <tr>
         <td id="rating-name">${data[i].name}</td>
         <td id="rating-description">${data[i].description}</td>
-        <td><button onclick="deleteRatingById(${data[i].id})" id="delete-movie" style="background-color:rgb(206, 87, 50)">🗑️</button></td>
+        <td><button onclick="deleteRatingById(${currentId},${data[i].id})" id="delete-movie" style="background-color:rgb(206, 87, 50)">🗑️</button>
+        <button onclick="updateCurrentId(${data[i].id});
+        openUpdateModal();showValues();" class="show-modal" id="update-movie" style="background-color:#1e81d3">
+          Update Rating
+        </button></td>
       </tr>
   `;
   }
   document.getElementById("rating-table").innerHTML = tableBody;
 };
 
-async function deleteRatingById(id) {
+async function deleteRatingById(movieId,id) {
   await fetch(baseUrl + `${movieId}/ratings/${id}`, { method: "delete" }).catch(
     (error) => {
       throw new Error("NaN");
     }
   );
-  getAllMovies();
+  
+  getAllRatings(movieId);
 }
 
 function selectedItem() {
-  var x = document.getElementById("movie-list").value;
-  console.log(x);
-  document.getElementById("div2").innerHTML = "You selected: " + x;
+  var x = document.getElementById("movies-list").options;
+  try {
+   if( x[x.selectedIndex].id !== 'empty' ){
+    getAllRatings(x[x.selectedIndex].id);
+    updateCurrentId(x[x.selectedIndex].id) }
+    else
+    {document.getElementById("rating-table").innerHTML='<tr></tr>'}
+  } catch (error) {
+    console.log("Empty Option")
+  }
+  
 }
+function updateCurrentId(id) {
+  currentId = id;
+}
+
